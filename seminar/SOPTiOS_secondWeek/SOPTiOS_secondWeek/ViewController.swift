ce91ec6df30b8b60bc59cf6759a40c1ba56dd5c5
YoonAh-dev <tlsdbsdk0525@naver.com>
2020-10-17 17:08:07 +0900

    [추가] 두 번째 세미나 실습 추가

---
+++ seminar/SOPTiOS_secondWeek/SOPTiOS_secondWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPTiOS_secondWeek
+//
+//  Created by SHIN YOON AH on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
