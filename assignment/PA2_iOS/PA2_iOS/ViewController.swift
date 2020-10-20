1b9e82c0844fcead36d19b249446ec2983d3fe26
YoonAh-dev <tlsdbsdk0525@naver.com>
2020-10-20 11:42:26 +0900

    [추가] 2번째 과제 추가

---
+++ assignment/PA2_iOS/PA2_iOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PA2_iOS
+//
+//  Created by SHIN YOON AH on 2020/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
