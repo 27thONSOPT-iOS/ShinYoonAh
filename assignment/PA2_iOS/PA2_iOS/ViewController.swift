995030e35cbedb4ce717de3c3938bf79cc7e3dc6
YoonAh-dev <tlsdbsdk0525@naver.com>
2020-10-20 12:33:54 +0900

    [추가] 2주차 도전과제 완료

--- assignment/PA2_iOS/PA2_iOS/ViewController.swift
+++ assignment/PA2_iOS/PA2_iOS/ViewController.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        buttonSetUp()
+    }
+    
+    func buttonSetUp() {
+        topButton.layer.cornerRadius = 25
+        topButton.isHidden = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if(scrollView.contentOffset.y > (scrollView.contentSize.height - scrollView.frame.size.height) / 2) {
+            topButton.isHidden = false
+        } else {
+            topButton.isHidden = true
+        }
+    }
+    
+    @IBAction func touchUpTop(_ sender: Any) {
+        topButton.isHidden = true
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
 }
 
