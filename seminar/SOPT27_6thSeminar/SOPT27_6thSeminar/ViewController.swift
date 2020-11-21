93db46f9f0c613f59d2934eb231092efee914337
YoonAh-dev <tlsdbsdk0525@naver.com>
2020-11-21 13:03:54 +0900

    [UPDATE] 6주차 세미나 추가

---
+++ seminar/SOPT27_6thSeminar/SOPT27_6thSeminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT27_6thSeminar
+//
+//  Created by SHIN YOON AH on 2020/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
